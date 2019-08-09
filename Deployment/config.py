113b7b2024c576f24a94f978a13609813058708b
thanzin10xds <50012786+thanzin10xds@users.noreply.github.com>
2019-08-09 12:25:16 +0530

    Model deployment config file

---
+++ Deployment/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+deployconfig = {
+    "load_model":"F:\\Projects\\Predicting_hospital_stay\\Code\\Modelling",
+    "create_csv":"F:\\Projects\\Predicting_hospital_stay\\Data\\Predicted_lengthofstay.csv",
+    "server_name":"10XDS-HW-255\\SQL2017",
+    "database_name":"TrialDB",
+    "schema_name":"DNA_HSP",
+    "master_table":"InpatientDetails"
+}
