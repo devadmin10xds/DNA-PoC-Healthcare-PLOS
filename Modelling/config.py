049135bc24882f24fd9b3e78aae483c0b39e42cc
thanzin10xds <50012786+thanzin10xds@users.noreply.github.com>
2019-08-09 12:24:17 +0530

    Modelling Config file

---
+++ Modelling/config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+modelconfig = {
+	"load_train": "F:\\Projects\\Predicting_hospital_stay\\Data\\data_preprocessed.csv",    
+    "save_model":"F:\\Projects\\Predicting_hospital_stay\\Code\\Modelling"
+}
