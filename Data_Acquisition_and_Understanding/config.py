aa2d2107d5b44c69df8e09e3164a4a6cc823cd09
thanzin10xds <50012786+thanzin10xds@users.noreply.github.com>
2019-08-09 12:22:46 +0530

    Data Preprocessing Config File

---
+++ Data_Acquisition_and_Understanding/config.py
@@ -0,0 +1,7 @@
+dataprepconfig = {
+	"create_csv": "F:\\Projects\\Predicting_hospital_stay\\Data\\data_preprocessed.csv",
+    "server_name":"10XDS-HW-255\\SQL2017",
+    "master_table":"InpatientDetails",
+    "database_name":"TrialDB",
+    "schema_name":"DNA_HSP"   
+}
